b5b2a13fa7e127e88969dd4ec06a6fab25f93d8a
ansouth <ansouth@qq.com>
2018-05-04 01:58:46 +0800

    impress,js

--- 选项卡/index.js
+++ 选项卡/index.js
@@ -1,9 +1,22 @@
-// function $(id) {
-//     return typeof  id==='string'?document.getElementById(id):id;
-// }
-window.onload=function (ev) {
-    //获取鼠标滑过或点击的标签和要切换的内容的元素
-    var titles =document.getElementById('notice-title');
-    var oUl    = titles.getElementsByClassName('li');
-     console.log(titles.length);
-};
+window.onload=function () {
+    var lis  = document.querySelectorAll('#notice-title ul li');
+    var divs = document.querySelectorAll('#notice-con div');
+    if(lis.length != divs.length){
+        return;
+    }
+
+    for(var i=0; i<lis.length; i++){
+        lis[i].id = i ;
+        lis[i].onmouseover =function () {
+            // alert(this.id);
+            //清楚所以li上的class
+            for (var j=0;j<lis.length;j++){
+                lis[j].className = '';
+                divs[j].style.display = 'none';
+            }
+            //设置当前为高亮显示
+            this.className ='select';
+            divs[this.id].style.display = 'block';
+        }
+    }
+}
